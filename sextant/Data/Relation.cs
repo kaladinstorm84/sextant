e1fd74dc731d225300e103314a2e4afa03e41500
Matthew Anderson <Matthew.Anderson@deltafs.co.uk>
2021-06-07 17:03:50 +0100

    Update mapping - and also switch to use internal connection to plantuml

---
+++ sextant/Data/Relation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sextant.Data
+{
+    public class Relation
+    {
+        public string RelationFrom { get; set; }
+        public string RelationTo { get; set; }
+        public RelationType Type { get; set; }
+    }
+    public enum RelationType
+    {
+        Single,
+        SingleLong,
+        Double,
+        DoubleLong,
+    }
+}
